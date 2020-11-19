6632ba5b34b8a93ba15915cf635bf41c329fcb70
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-11-19 12:39:44 +0000

    Check last entry and only insert new documents

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import pandas as pd
 import flask
 from flask import request, make_response
 import os
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, DESCENDING
 import ast
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -44,14 +44,24 @@ def upload_file():
 
         f = request.files['upload_file']
         name = f.filename
+        name = name[name.index('(') + 1:name.index('+') - 1 if '+' in name else name.index(')')]
         data = pd.read_csv(f, sep='\t')
-        name = name[name.index('(')+1:name.index('+')-1 if '+' in name else name.index(')')]
-        readings.insert_many({
-                                 'name': name,
-                                 'time': pd.to_datetime(row['Unnamed: 0']).isoformat(),
-                                 **{k: ast.literal_eval(str(v)) for k, v in row.items()
-                                    if pd.notna(v) and str(v).replace('.', '', 1).isdigit()}
-                             } for row in data.to_dict('records') if pd.notna(row['Unnamed: 0']))
+
+        # Get the latest inserted time
+        last_entry = readings.find_one(
+            {'name': name}, {'time': 1},
+            sort=[('_id', DESCENDING)]
+        )
+        if last_entry is not None:
+            last_time = pd.to_datetime(last_entry['time'])
+            data = data[pd.to_datetime(data['Unnamed: 0']) > last_time]
+        if len(data) > 0:
+            readings.insert_many({
+                                     'name': name,
+                                     'time': pd.to_datetime(row['Unnamed: 0']).isoformat(),
+                                     **{k: ast.literal_eval(str(v)) for k, v in row.items()
+                                        if pd.notna(v) and str(v).replace('.', '', 1).isdigit()}
+                                 } for row in data.to_dict('records') if pd.notna(row['Unnamed: 0']))
 
         return make_response({}, 200)
 
